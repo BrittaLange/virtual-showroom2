bc7ad5a4bf4371073471a0d8779836402e109ce0
BrittaLange <britta.lange@mailbox.org>
2023-07-03 19:42:40 +0200

    Added FactbookPage.

---
+++ src/main/java/pageObjects/FactbookPage.java
@@ -0,0 +1,32 @@
+package pageObjects;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import base.BasePage;
+
+public class FactbookPage extends BasePage {
+
+	private WebDriver driver;
+	private By addCustomerBtn = By.cssSelector("mat-grid-tile#add_customer > .mat-grid-tile-content");
+	
+
+
+	public FactbookPage() throws IOException {
+		super();
+		this.driver = getDriver();
+	}
+
+	public String getCurrentUrl() {
+		System.out.println(driver.getCurrentUrl());
+		return driver.getCurrentUrl();
+	}
+	
+	public WebElement getAddCustomerBtn() {
+		return driver.findElement(addCustomerBtn);
+	}
+
+}
