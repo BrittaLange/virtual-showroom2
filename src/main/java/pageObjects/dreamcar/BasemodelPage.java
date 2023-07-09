64c9eedc625c761e24201a400fb5cf7e2b515bc4
BrittaLange <britta.lange@mailbox.org>
2023-07-09 20:23:34 +0200

    Added field and getter for price label.

--- src/main/java/pageObjects/dreamcar/BasemodelPage.java
+++ src/main/java/pageObjects/dreamcar/BasemodelPage.java
@@ -12,7 +12,8 @@ public class BasemodelPage extends NavigationTabs {
 	private By basemodelCombobox = By.cssSelector("mat-select#select_basemodel");
 	private By basemodelItem = By.id("mat-select-value-1");
 	private By option10 = By.id("option_basemodel_10");
-	
+	private By priceLabel = By.id("label_price_component");
+
 	public BasemodelPage() throws IOException {
 		super();
 		this.driver = getDriver();
@@ -21,18 +22,21 @@ public class BasemodelPage extends NavigationTabs {
 	public String getCurrentUrl() {
 		return driver.getCurrentUrl();
 	}
-	
+
 	public WebElement getBasemodelCombobox() {
 		return driver.findElement(basemodelCombobox);
 	}
-	
+
 	public WebElement getBasemodelItem() {
 		return driver.findElement(basemodelItem);
 	}
-	
+
 	public WebElement getOption10() {
 		return driver.findElement(option10);
 	}
 
-	
+	public WebElement getPriceLabel() {
+		return driver.findElement(priceLabel);
+	}
+
 }
