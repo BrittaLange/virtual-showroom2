765260f3cce79d9517304e658f5e76a5facb7ee5
BrittaLange <britta.lange@mailbox.org>
2023-07-11 21:30:06 +0200

    Added fields and getters.

--- src/main/java/pageObjects/dreamcar/ExtrasPage.java
+++ src/main/java/pageObjects/dreamcar/ExtrasPage.java
@@ -10,13 +10,27 @@ public class ExtrasPage extends NavigationTabs {
 
 	private WebDriver driver;
 	private By autopilot = By.id("checkbox_extra_0-input");
+	private By priceAutopilot = By.id("extra_price_0");
+	private By autopilotLabel = By.cssSelector("[for='checkbox_extra_0-input'] .mat-checkbox-label");
 	private By floormats = By.id("checkbox_extra_3-input");
-	private By lighting = By.id("checkbox_extra_7-input");
+	private By priceFloormats = By.id("extra_price_3");
+	private By floormatsLabel = By.cssSelector("[for='checkbox_extra_3-input'] .mat-checkbox-label");
+	private By lighting = By.id("checkbox_extra_7");
+	private By lightingLabel = By.cssSelector("[for='checkbox_extra_7-input'] .mat-checkbox-label");
+	private By priceLighting = By.id("extra_price_7");
 	private By leatherwheel = By.id("checkbox_extra_4-input");
+	private By leatherwheelLabel = By.cssSelector("[for='checkbox_extra_4-input'] .mat-checkbox-label");
 	private By navigationsystem = By.id("checkbox_extra_2-input");
+	private By priceNavigationsystem = By.id("extra_price_2");
+	private By navigationsystemLabel = By.cssSelector("[for='checkbox_extra_2-input'] .mat-checkbox-label");
 	private By soundsystem = By.id("checkbox_extra_5-input");
+	private By priceSoundsystem = By.id("extra_price_5");
+	private By soundsystemLabel = By.cssSelector("[for='checkbox_extra_5-input'] .mat-checkbox-label");
 	private By sportssuspension = By.id("checkbox_extra_6-input");
+	private By priceSportssuspension = By.id("extra_price_6");
+	private By sportssuspensionLabel = By.cssSelector("[for='checkbox_extra_6-input'] .mat-checkbox-label");
 	private By wintertires = By.id("checkbox_extra_1-input");
+	private By wintertiresLabel = By.cssSelector("[for='checkbox_extra_1-input'] .mat-checkbox-label");
 	private By priceWinterTires = By.id("extra_price_1");
 
 	public ExtrasPage() throws IOException {
@@ -31,27 +45,90 @@ public class ExtrasPage extends NavigationTabs {
 	public WebElement getAutopilot() {
 		return driver.findElement(autopilot);
 	}
+	
+	public WebElement getPriceAutopilot() {
+		return driver.findElement(priceAutopilot);
+	}
+	
+	public WebElement getAutopilotLabel() {
+		return driver.findElement(autopilotLabel);
+	}
+	
 	public WebElement getFloormats() {
 		return driver.findElement(floormats);
 	}
+	
+	public WebElement getPriceFloormats() {
+		return driver.findElement(priceFloormats);
+	}
+	public WebElement getFloormatsLabel() {
+		return driver.findElement(floormatsLabel);
+	}
+	
 	public WebElement getLighting() {
 		return driver.findElement(lighting);
 	}
+	
+	public WebElement getPriceLighting() {
+		return driver.findElement(priceLighting);
+	}
+	
+	public WebElement getLightingLabel() {
+		return driver.findElement(lightingLabel);
+	}
+	
 	public WebElement getLeatherwheel() {
 		return driver.findElement(leatherwheel);
 	}
+	
+	public WebElement getLeatherwheelLabel() {
+		return driver.findElement(leatherwheelLabel);
+	}
+	
 	public WebElement getNavigationsystem() {
 		return driver.findElement(navigationsystem);
 	}
+	
+	public WebElement getPriceNavigationsystem() {
+		return driver.findElement(priceNavigationsystem);
+	}
+	
+	public WebElement getNavigationsystemLabel() {
+		return driver.findElement(navigationsystemLabel);
+	}
+	
 	public WebElement getSoundsystem() {
 		return driver.findElement(soundsystem);
 	}
+	
+	public WebElement getPriceSoundsystem() {
+		return driver.findElement(priceSoundsystem);
+	}
+	
+	public WebElement getSoundsystemLabel() {
+		return driver.findElement(soundsystemLabel);
+	}
+	
 	public WebElement getSportssuspension() {
 		return driver.findElement(sportssuspension);
 	}
+	
+	public WebElement getPriceSportssuspension() {
+		return driver.findElement(priceSportssuspension);
+	}
+	
+	public WebElement getSportssuspensionLabel() {
+		return driver.findElement(sportssuspensionLabel);
+	}
+	
 	public WebElement getWintertires() {
 		return driver.findElement(wintertires);
 	}
+	
+	public WebElement getWintertiresLabel() {
+		return driver.findElement(wintertiresLabel);
+	}
+	
 	public WebElement getPriceWinterTires() {
 		return driver.findElement(priceWinterTires);
 	}
