336e0c9db00db56e6ce9c54d3be69ed68ca1232f
BrittaLange <britta.lange@mailbox.org>
2023-07-12 13:55:51 +0200

    Added fields and getters.

--- src/main/java/pageObjects/dreamcar/ColorPage.java
+++ src/main/java/pageObjects/dreamcar/ColorPage.java
@@ -12,8 +12,10 @@ public class ColorPage extends NavigationTabs {
 	private By lightblue = By.id("colorLight blue-input");
 	private By priceColor = By.id("label_price_component");
 	private By effectSelect = By.cssSelector("#select_coloreffekt");
+	private By selectedEffect = By.xpath("//div[@id='mat-select-value-5']/span/span[.='Pearl effect (1.999,00 €)']");
 	private By pearlEffect = By.id("option_coloreffect_2");
 	private By totalAmount = By.id("label_price_sum");
+	private By labelLightBlue = By.cssSelector("[for='colorLight blue-input'] .mat-radio-label-content");
 
 	public ColorPage() throws IOException {
 		super();
@@ -28,21 +30,27 @@ public class ColorPage extends NavigationTabs {
 		return driver.findElement(lightblue);
 	}
 
-	
 	public WebElement getPriceColor() {
 		return driver.findElement(priceColor);
 	}
-	
+
 	public WebElement getEffectSelect() {
 		return driver.findElement(effectSelect);
 	}
 	
+	public WebElement getSelectedEffect() {
+		return driver.findElement(selectedEffect);
+	}
+
 	public WebElement getPearlEffect() {
 		return driver.findElement(pearlEffect);
 	}
 
-	
 	public WebElement getTotalAmount() {
 		return driver.findElement(totalAmount);
+	}
+	
+	public WebElement getLabelLightBlue() {
+		return driver.findElement(labelLightBlue);
 	}
 }
